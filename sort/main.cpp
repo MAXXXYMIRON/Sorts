baada78789e7667831e8d700622ed712f725d6d5
MAXXXYMIRON <klavioturasucka2@gmail.com>
2020-05-08 19:55:30 +0300

    sort_0.1

--- sort/main.cpp
+++ sort/main.cpp
@@ -2,17 +2,9 @@
 #include <stdio.h>
 #include <time.h>
 #include"Sort.h"
+#include <fstream>
 
-template<class t>
-void out(t arr[], int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		std::cout << arr[i] << "   ";
-	}
-	std::cout << std::endl;
-}
-
+using namespace std;
 
 template<class t>
 void Rand(t arr[], int len)
@@ -22,59 +14,62 @@ void Rand(t arr[], int len)
 		arr[i] = (-1000 + rand() % 2000) / 100.;
 	}
 }
-void Tests(int Length)
+
+
+void Tests(int len, string nameFile)
 {
-	double* Test = new double[Length];
+	double* Test = new double[len];
 
+	ofstream file;
+	file.open(nameFile);
 
-	std::cout << "Wait))" << std::endl;
-	Rand(Test, Length);
-	std::cout << "Selection Sotr - " << Length << std::endl;
+	file << "Wait))" << "\n";
+	Rand(Test, len);
+	file << "Quick Sort - " << len << "\n";
 	unsigned int start = clock();
-	SelectionSort(Test, Length);
+	QuickSort(Test, 0, len - 1);
 	unsigned int end = clock();
-	std::cout << (double)(end - start) / CLOCKS_PER_SEC << 'c' << std::endl << std::endl << std::endl;
-	std::cout << std::endl;
+	file << (double)(end - start) / CLOCKS_PER_SEC << 'c' << "\n" << "\n" << "\n";
+	file << "\n";
 
-
-	std::cout << "Wait))" << std::endl;
-	Rand(Test, Length);
-	std::cout << "Bubble Sort - " << Length << std::endl;
+	file << "Wait))" << "\n";
+	Rand(Test, len);
+	file << "Selection Sotr - " << len << "\n";
 	start = clock();
-	BubbleSort(Test, Length);
+	SelectionSort(Test, len);
 	end = clock();
-	std::cout << (double)(end - start) / CLOCKS_PER_SEC << 'c' << std::endl << std::endl << std::endl;
-	std::cout << std::endl;
+	file << (double)(end - start) / CLOCKS_PER_SEC << 'c' << "\n" << "\n" << "\n";
+	file << "\n";
 
 
-	std::cout << "Wait))" << std::endl;
-	Rand(Test, Length);
-	std::cout << "Insertion Sort - " << Length << std::endl;
+	file << "Wait))" << "\n";
+	Rand(Test, len);
+	file << "Bubble Sort - " << len << "\n";
 	start = clock();
-	InsertionSort(Test, Length);
+	BubbleSort(Test, len);
 	end = clock();
-	std::cout << (double)(end - start) / CLOCKS_PER_SEC << 'c' << std::endl << std::endl << std::endl;
-	std::cout << std::endl;
+	file << (double)(end - start) / CLOCKS_PER_SEC << 'c' << "\n" << "\n" << "\n";
+	file << "\n";
 
 
-	std::cout << "Wait))" << std::endl;
-	Rand(Test, Length);
-	std::cout << "Quick Sort - " << Length << std::endl;
+	file << "Wait))" << "\n";
+	Rand(Test, len);
+	file << "Insertion Sort - " << len << "\n";
 	start = clock();
-	QuickSort(Test, 0, Length - 1);
+	InsertionSort(Test, len);
 	end = clock();
-	std::cout << (double)(end - start) / CLOCKS_PER_SEC << 'c' << std::endl << std::endl << std::endl;
-	std::cout << std::endl;
+	file << (double)(end - start) / CLOCKS_PER_SEC << 'c' << "\n" << "\n" << "\n";
+	file << "\n";
 
 
-	std::cout << "Wait))" << std::endl;
-	Rand(Test, Length);
-	std::cout << "Shell Sort - " << Length << std::endl;
+	file << "Wait))" << "\n";
+	Rand(Test, len);
+	file << "Shell Sort - " << len << "\n";
 	start = clock();
-	ShellSort(Test, Length);
+	ShellSort(Test, len);
 	end = clock();
-	std::cout << (double)(end - start) / CLOCKS_PER_SEC << 'c' << std::endl << std::endl << std::endl;
-	std::cout << std::endl;
+	file << (double)(end - start) / CLOCKS_PER_SEC << 'c' << "\n" << "\n" << "\n";
+	file << "\n";
 }
 
 
@@ -82,8 +77,12 @@ void main()
 {
 	setlocale(0, "");
 
-	unsigned Length = 10;
+	unsigned len = 10;
 
+	ofstream file;
+	file.open("Tests.txt");
+
+	//Сортировка на малых массивах заполненных вручную
 	double A[] = {2, 6, 1, 8, 4, 8, 3, 2, 9, 1};
 	double B[] = {3, 5, 6, 21, 7, 4, 8, 3, 7, 2};
 	double C[] = {12, 5, 2, 5, 23, 65, 2, 0, 2, 32};
@@ -91,41 +90,98 @@ void main()
 	double E[] = {54, 3, 22, 6, 2, 66, 9, 0, 0, 122};
 
 
-	std::cout << "Selection Sotr" << std::endl;
-	out(A, Length);
-	SelectionSort(A, Length);
-	out(A, Length);
-	std::cout << std::endl;
+	//Сортировка массива А
+	file << "Selection Sotr" << "\n";
+	for (int i = 0; i < len; i++)
+	{
+		file << A[i] << "   ";
+	}
+	file << "\n";
+	SelectionSort(A, len);
+	for (int i = 0; i < len; i++)
+	{
+		file << A[i] << "   ";
+	}
+	file << "\n";
+	file << "\n";
 
-	std::cout << "Bubble Sort" << std::endl;
-	out(B, Length);
-	BubbleSort(B, Length);
-	out(B, Length);
-	std::cout << std::endl;
 
-	std::cout << "Insertion Sort" << std::endl;
-	out(C, Length);
-	InsertionSort(C, Length);
-	out(C, Length);
-	std::cout << std::endl;
 
-	std::cout << "Quick Sort" << std::endl;
-	out(D, Length);
-	QuickSort(D, 0, Length - 1);
-	out(D, Length);
-	std::cout << std::endl;
 
-	std::cout << "Shell Sort" << std::endl;
-	out(E, Length);
-	ShellSort(E, Length);
-	out(E, Length);
-	std::cout << std::endl;
+	//Сортировка массива B
+	file << "Bubble Sort" << "\n";
+	for (int i = 0; i < len; i++)
+	{
+		file << B[i] << "   ";
+	}
+	file << "\n";
+	BubbleSort(B, len);
+	for (int i = 0; i < len; i++)
+	{
+		file << B[i] << "   ";
+	}
+	file << "\n";
+	file << "\n";
 
-	
-	Tests(10000);
-	Tests(20000);
-	Tests(30000);
-	Tests(40000);
+
+
+	//Сортировка массива C
+	file << "Insertion Sort" << "\n";
+	for (int i = 0; i < len; i++)
+	{
+		file << C[i] << "   ";
+	}
+	file << "\n";
+	InsertionSort(C, len);
+	for (int i = 0; i < len; i++)
+	{
+		file << C[i] << "   ";
+	}
+	file << "\n";
+	file << "\n";
+
+
+
+	//Сортировка массива D
+	file << "Quick Sort" << "\n";
+	for (int i = 0; i < len; i++)
+	{
+		file << D[i] << "   ";
+	}
+	file << "\n";
+	QuickSort(D, 0, len - 1);
+	for (int i = 0; i < len; i++)
+	{
+		file << D[i] << "   ";
+	}
+	file << "\n";
+	file << "\n";
+
+
+
+	//Сортировка массива E
+	file << "Shell Sort" << "\n";
+	for (int i = 0; i < len; i++)
+	{
+		file << E[i] << "   ";
+	}
+	file << "\n";
+	ShellSort(E, len);
+	for (int i = 0; i < len; i++)
+	{
+		file << E[i] << "   ";
+	}
+	file << "\n";
+	file << "\n";
+
+
+
+	//Тестирование больших массивов, заполненных рандомно
+	//со снятием временных показателей
+	Tests(10000, "Test10000.txt");
+	Tests(20000, "Test20000.txt");
+	Tests(30000, "Test30000.txt");
+	Tests(40000, "Test40000.txt");
 
 	system("pause");
 }
